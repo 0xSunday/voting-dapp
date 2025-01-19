39df495ebca0dc5767bf8e309b66110dafce6a1a
Your Name <you@example.com>
2025-01-19 12:43:09 +0000

    fix 3

--- src/components/cluster/cluster-ui.tsx
+++ src/components/cluster/cluster-ui.tsx
@@ -100,14 +100,14 @@ export function ClusterUiModal({ hideModal, show }: { hideModal: () => void; sho
     >
       <input
         type="text"
-        votingdapp="Name"
+        // votingdapp="Name"
         className="input input-bordered w-full"
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
       <input
         type="text"
-        votingdapp="Endpoint"
+        // votingdapp="Endpoint"
         className="input input-bordered w-full"
         value={endpoint}
         onChange={(e) => setEndpoint(e.target.value)}
