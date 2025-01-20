64f852cf65f689215022d999a23aaa5f95ad060f
Your Name <sunday7637@gmail.com>
2025-01-20 14:04:34 +0000

    contribution check fix

--- anchor/programs/voting/src/lib.rs
+++ anchor/programs/voting/src/lib.rs
@@ -1,13 +1,12 @@
 use anchor_lang::prelude::*;
 
 declare_id!("6z68wfurCMYkZG51s1Et9BJEd9nJGUusjHXNt4dGbNNF");
-
+// check fix 
 #[program]
 pub mod voting {
     use super::*;
 
    
-}
 
 #[derive(Accounts)]
 pub struct Initialize {}
