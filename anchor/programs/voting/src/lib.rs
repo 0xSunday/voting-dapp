763f5f6024ecf887ebfda9f71c7060efc3122eea
Your Name <sunday7637@gmail.com>
2025-01-21 17:21:48 +0000

    basic initilization

--- anchor/programs/voting/src/lib.rs
+++ anchor/programs/voting/src/lib.rs
@@ -1,12 +1,53 @@
-use anchor_lang::prelude::*;
+use anchor_lang::{prelude::*, solana_program::address_lookup_table::instruction};
 
 declare_id!("6z68wfurCMYkZG51s1Et9BJEd9nJGUusjHXNt4dGbNNF");
-// check fix 
+
 #[program]
 pub mod voting {
     use super::*;
 
-   
+    pub fn Initialize_poll(
+        ctx: Context<InitializePoll>,
+        poll_id: u64,
+        description: String,
+        poll_start: u64,
+        poll_end: u64,
+    ) -> Result<()> {
+        let poll = &mut ctx.accounts.poll;
+        poll.poll_id = poll_id;
+        poll.description = description;
+        poll.poll_start = poll_start;
+        poll.poll_end = poll_end;
+        poll.candidate_amount = 0;
 
+        Ok(())
+    }
+}
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(poll_id:u64)]
+pub struct InitializePoll<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+        init,
+        payer=signer,
+        space = 8 + Poll::INIT_SPACE,
+        seeds = [poll_id.to_le_bytes().as_ref()],
+        bump,
+
+    )]
+    pub poll: Account<'info, Poll>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Poll {
+    pub poll_id: u64,
+    #[max_len(280)]
+    pub description: String,
+    pub poll_start: u64,
+    pub poll_end: u64,
+    pub candidate_amount: u64,
+}
+// pub struct Initialize {}
