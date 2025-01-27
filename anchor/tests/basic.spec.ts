85d5e24fde69050e06f6792b5ffc1c827361da73
Your Name <sunday7637@gmail.com>
2025-01-27 16:24:32 +0000

    fix 6

--- anchor/tests/basic.spec.ts
+++ anchor/tests/basic.spec.ts
@@ -12,7 +12,6 @@ const IDL = require("../target/idl/voting.json");
 
 describe("Voting", () => {
   it("initilize poll", async () => {
-    // test test test 
-    // adding more tests
+   
   });
 });
